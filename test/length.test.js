34b1986acf9cb70b397edf0f1aac88435199716d
George Ornbo <george@shapeshed.com>
2012-09-24 15:04:39 +0100

    add length and abstract messages

---
+++ test/length.test.js
@@ -0,0 +1,57 @@
+
+var assert = require('assert'),
+    validotron = require('../lib/validotron'),
+    test_helper = require('./support/test_helper');
+
+
+describe('length', function(done){
+  it('should pass validation', function(done){
+    var validation = new validotron({ 
+      name: { 
+        data: "foo", 
+        length: {
+          minimum: 2
+        } 
+      }
+    });
+    assert.strictEqual(test_helper.isEmpty(validation.errors), true);
+    done();
+  })
+  it('should populate the errors object with the minimum constraint set', function(done){
+    var validation = new validotron({ 
+      name: { 
+        data: "foo", 
+        length: {
+          minimum: 20
+        } 
+      }
+    });
+    assert.strictEqual(JSON.stringify(validation.errors), "{\"name\":[\"is too short (min is 20 characters)\"]}");
+    done();
+  })
+  it('should populate the errors object with the maximum constraint set', function(done){
+    var validation = new validotron({ 
+      name: { 
+        data: "foobarbaz", 
+        length: {
+          maximum: 6
+        } 
+      }
+    });
+    assert.strictEqual(JSON.stringify(validation.errors), "{\"name\":[\"is too long (max is 6 characters)\"]}");
+    done();
+  })
+  it('should populate the errors object with the is constraint set', function(done){
+    var validation = new validotron({ 
+      name: { 
+        data: "foobarbaz", 
+        length: {
+          is: 6
+        } 
+      }
+    });
+    assert.strictEqual(JSON.stringify(validation.errors), "{\"name\":[\"is the wrong length (should be 6 characters)\"]}");
+    done();
+  })
+})
+
