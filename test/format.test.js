29ce5a5bfb2d1af0c2342cf907b4d2da9bde7cc2
Sven Lito <me@svenlito.com>
2013-02-17 15:08:57 +0000

    but does it lint?
    yeap

--- test/format.test.js
+++ test/format.test.js
@@ -3,54 +3,61 @@ var assert = require('assert'),
     test_helper = require('./support/test_helper');
 
 
-describe('format', function(done){
-  it('should pass validation', function(done){
-    var validation = new validotron({ 
-      name: { 
-        data: "ABC", 
+describe('format', function(done) {
+
+  'use strict';
+
+  it('should pass validation', function(done) {
+    var validation = new validotron({
+      name: {
+        data: "ABC",
         format: {
           with: "[A-Z]"
-        } 
+        }
       }
     });
     assert.strictEqual(test_helper.isEmpty(validation.errors), true);
     done();
-  })
-  it('should populate the errors object when validation fails', function(done){
-    var validation = new validotron({ 
-      name: { 
-        data: "^%$#", 
+  });
+
+  it('should populate the errors object when validation fails', function(done) {
+    var validation = new validotron({
+      name: {
+        data: "^%$#",
         format: {
           with: "[A-Z]"
-        } 
+        }
       }
     });
     assert.strictEqual(JSON.stringify(validation.errors), "{\"name\":[\"is invalid\"]}");
     done();
-  })
-  it('should set a custom failure message correctly', function(done){
-    var validation = new validotron({ 
-      name: { 
-        data: "^%$#", 
+  });
+
+  it('should set a custom failure message correctly', function(done) {
+    var validation = new validotron({
+      name: {
+        data: "^%$#",
         format: {
           with: "[A-Z]",
           message: "the regex did not match"
-        } 
+        }
       }
     });
     assert.strictEqual(JSON.stringify(validation.errors), "{\"name\":[\"the regex did not match\"]}");
     done();
-  })
-  it('should pass validation if allow_blank: is true', function(done){
-    var validation = new validotron({ 
-      name: { 
-        data: "", 
+  });
+
+  it('should pass validation if allow_blank: is true', function(done) {
+    var validation = new validotron({
+      name: {
+        data: "",
         format: {
-          allow_blank: true 
+          allow_blank: true
         },
       }
     });
     assert.strictEqual(test_helper.isEmpty(validation.errors), true);
     done();
-  })
-})
+  });
+
+});
